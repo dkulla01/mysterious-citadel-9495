5d2e2c79818a81b65e4040ee3da42e78527ede59
Dan Kass <romeboards@wr-130-64-188-237.medford.tufts.edu>
2013-03-31 23:12:47 -0400

    second

--- index.php
+++ index.php
@@ -242,7 +242,7 @@ $app_name = idx($app_info, 'name', '');
       </div>
       <?php } else { ?>
       <div>
-        <h1>Welcome</h1>
+        <h1>Welcome, you tweaking fool.</h1>
         <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
       </div>
       <?php } ?>
