f20948bac763725d4e381ce4f2931f11b3765c5f
Dan Kass <romeboards@resnet140-108.medford.tufts.edu>
2013-04-08 14:02:52 -0400

    another test

--- index.php
+++ index.php
@@ -248,28 +248,6 @@ $app_name = idx($app_info, 'name', '');
           ?>
         </ul>
       </div>
- 
-      <div class="list">
-        <h3>A few of your friends</h3>
-        <ul class="friends">
-          <?php
-            foreach ($friends as $friend) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($friend, 'id');
-              $name = idx($friend, 'name');
-          ?>
-          <li>
-            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-              <?php echo he($name); ?>
-            </a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
- 
       
     </section>
 
