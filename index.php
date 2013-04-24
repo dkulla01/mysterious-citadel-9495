7fe1c202356fd691503528f268ce1d9818538f7b
Dan Kass <romeboards@wr-130-64-188-54.medford.tufts.edu>
2013-04-23 22:05:17 -0400

    another test

--- index.php
+++ index.php
@@ -70,7 +70,8 @@ if ($user_id) {
   foreach ($friends as $friend) {
 	  
       $id = idx($friend, 'id');	
-      print_r($friend);
+      print_r($friend['links']);
+      echo('\n\n');
       //$query = '/' . $id . 'links?limit=1';
       //$link = idx($facebook->api($query), 'data', array());
       
