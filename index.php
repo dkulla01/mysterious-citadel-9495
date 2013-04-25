64429bc64190368acaabf520dcb07a8c943918cf
Dan Kass <romeboards@wr-130-64-188-253.medford.tufts.edu>
2013-04-24 22:11:32 -0400

    another test

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 // Defined in 'AppInfo.php'
 require_once('AppInfo.php');
 
-require_once('FriendRank.php');
+/* require_once('FriendRank.php'); */
+require_once('friendrank.php');
 
 // Enforce https on production
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
