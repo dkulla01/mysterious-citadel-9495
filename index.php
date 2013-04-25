23220e9fab62c97ca26b4e5d99faa20f7b821e0c
Dan Kass <romeboards@wr-130-64-188-253.medford.tufts.edu>
2013-04-24 22:58:49 -0400

    another test

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ foreach ($holyshit as $trick) {
  	$name = $trick['name'];
  	$score = $trick['score'];
  	$inbox_score = $trick['weight']['inbox_chat'];
- 	echo($name . '('. $inbox_score . ') / ');
+ 	echo($name . '('. $score . ') / ');
   /*
   $id = idx($friend, 'id');	
   echo($id . ' ');
