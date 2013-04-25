cbcf421ae517587c6e050967e24365273df03605
Dan Kass <romeboards@wr-130-64-188-58.medford.tufts.edu>
2013-04-25 18:18:16 -0400

    another test

--- Aggregator.php
+++ Aggregator.php
@@ -26,9 +26,9 @@ class LinkAggregator {
 				//print_r($friend);
 				$link = $this->getFriendLinks($friend['uid']);
 				//$i++;
-				if(!empty($link)) {				//some people return nothing
-					$links[$friend['uid']] = $link;
-				 	//echo('<p>' . $i . ': ' . $friend['name'] . '- ' . $link['title'] . '</p>');
+				if(!empty($link)) {				//some people return nothing, randomly
+					//$links[$friend['uid']] = $link;
+				 	echo('<p>' . $i . ': ' . $friend['name'] . '- ' . $link['title'] . '</p>');
 					$i++;
 				}	
 			}		
